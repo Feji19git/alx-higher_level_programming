ee5e567d74a4526c7405d13532dc809a6adb89e9
Feji19git <69316141+Feji19git@users.noreply.github.com>
2023-03-18 17:31:12 +0100

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A module that defines a class
+"""
+
+
+class Square:
+    """
+    A Class that defines a square
+    """
+
+    def __init__(self, size):
+        """
+        args:
+            size = defines the size of the square
+        """
+        self.__size = size
